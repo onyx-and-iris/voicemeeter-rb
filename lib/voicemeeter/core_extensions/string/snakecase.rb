48b9e931c4e86d101440cf0101ff23243b0f6fc7
onyx-and-iris <code@onyxandiris.online>
2023-07-09 23:48:46 +0100

    extend base String class to add a snakecase method

---
+++ lib/voicemeeter/core_extensions/string/snakecase.rb
@@ -0,0 +1,19 @@
+module Voicemeeter
+  module Ext
+    module String
+      module SnakeCase
+        class ::String
+          def snakecase
+            self
+              .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+              .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+              .tr("-", "_")
+              .gsub(/\s/, "_")
+              .gsub(/__+/, "_")
+              .downcase
+          end
+        end
+      end
+    end
+  end
+end
