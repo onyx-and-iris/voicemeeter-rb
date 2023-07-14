aa159d94144436fee8484f20f84b68d78b03348b
onyx-and-iris <code@onyxandiris.online>
2023-07-14 11:45:08 +0100

    run through standard

--- lib/voicemeeter/core_extensions/string/snakecase.rb
+++ lib/voicemeeter/core_extensions/string/snakecase.rb
@@ -4,8 +4,7 @@ module Voicemeeter
       module SnakeCase
         class ::String
           def snakecase
-            self
-              .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+            gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
               .gsub(/([a-z\d])([A-Z])/, '\1_\2')
               .tr("-", "_")
               .gsub(/\s/, "_")
