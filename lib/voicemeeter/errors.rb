077a14fe62f45620f04d6c9bc3fe0db2041abb98
onyx-and-iris <code@onyxandiris.online>
2023-07-15 00:16:35 +0100

    VMCAPIError now takes fn and res as args

--- lib/voicemeeter/errors.rb
+++ lib/voicemeeter/errors.rb
@@ -1,9 +1,26 @@
+require_relative "core_extensions/string/camelcase"
+
 module Voicemeeter
   module Errors
     class VMError < StandardError
     end
 
     class VMCAPIError < VMError
+      attr_reader :code
+
+      def initialize(ruby_name, code)
+        @ruby_name = ruby_name
+        @code = code
+        super(message)
+      end
+
+      def message
+        "#{fn_name} returned #{code}"
+      end
+
+      def fn_name
+        "VBVMR_#{@ruby_name.to_s.delete_prefix("bind_").camelcase.sub("macro_button", "macrobutton")}"
+      end
     end
   end
 end
