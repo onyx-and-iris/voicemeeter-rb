a87e626ff1c6bff869378562030eff30e0dbdbca
onyx-and-iris <code@onyxandiris.online>
2023-07-18 19:35:53 +0100

    string param lower test added

--- test/lower/lower_test.rb
+++ test/lower/lower_test.rb
@@ -5,11 +5,16 @@ class SetGetParamTest < Minitest::Test
     Test.vm.set("Fx.Reverb.On", 1)
     assert_equal(1, Test.vm.get("Fx.Reverb.On"))
   end
+
+  def test_it_sets_and_gets_string_parameter
+    Test.vm.set("Strip[0].Label", "strip0name")
+    assert_equal("strip0name", Test.vm.get("Strip[0].Label", true))
+  end
 end
 
 class SetGetButtonTest < Minitest::Test
   def test_it_sets_and_gets_macrobutton_parameter
-    Test.vm.set_buttonstatus(0, 3, 1)   # button0, trigger, true
+    Test.vm.set_buttonstatus(0, 3, 1)   # button[0], trigger, true
     assert_equal(1, Test.vm.get_buttonstatus(0, 3))
   end
 end
