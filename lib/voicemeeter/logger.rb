333d8da67844106f91c5d8c4e1bfb2ca19fa43da
onyx-and-iris <code@onyxandiris.online>
2023-07-25 12:58:32 +0100

    fixes printing Module name

--- lib/voicemeeter/logger.rb
+++ lib/voicemeeter/logger.rb
@@ -4,7 +4,7 @@ module Voicemeeter
   module Logging
     def logger
       @logger = Logger.new($stdout, level: ENV.fetch("VM_LOG_LEVEL", "WARN"))
-      @logger.progname = self.class.name
+      @logger.progname = instance_of?(::Module) ? name : self.class.name
       @logger
     end
   end
